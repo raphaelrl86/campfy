f0e0da95bd630ea1e8e6bd3fc2b738b994cd6e63
raphaelrl86 <58190753+raphaelrl86@users.noreply.github.com>
2023-02-25 11:08:19 -0300

    Add files via upload

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,10 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+
+const SignUpPage = () => {
+    return (  );
+}
+ 
+export default SignUpPage;
