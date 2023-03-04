59ef4a0fd8adbd12a72b9b7d0125e5ef159c106e
Carolina Barbosa <carolina.bs.ribeiro@gmail.com>
2023-03-04 16:46:37 -0300

    altera auth context, theme context e página de usuário para aceitar apenas usuários logado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,36 @@
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
+import {useContext} from 'react'
+import {ThemeContext} from '../context/theme.context'
+import {AuthContext} from '../context/auth.context'
 
 const Navbar = () => {
     
+  const {theme, toggleTheme} = useContext(ThemeContext)
+  const {logout} = useContext(AuthContext)
+
     return ( 
-        <nav className="navbar bg-light">
+
+        <nav className={`navbar bg-${theme}`}>
+
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/"> 
+            <NavLink className="navbar-brand" to="/"> 
               {/* <img src= {DiceImage} alt="Logo de dados" width="30" height="24" className="d-inline-block align-text-top"/> */}
               <span class="navbar-text">  Página inicial</span>
-            </Link> 
-            <Link className="navbar-brand" to="/signup"> 
+            </NavLink> 
+            <NavLink className="navbar-brand" to="/signup"> 
               {/* <img src= {DiceImage} alt="Logo de dados" width="30" height="24" className="d-inline-block align-text-top"/> */}
               <span class="navbar-text">  SignUp </span>
-            </Link> 
-            <Link className="navbar-brand" to="/login"> 
+            </NavLink> 
+            <NavLink className="navbar-brand" to="/login"> 
               {/* <img src= {DiceImage} alt="Logo de dados" width="30" height="24" className="d-inline-block align-text-top"/> */}
               <span class="navbar-text">  Login </span>
-            </Link> 
-            
+            </NavLink> 
           </div>
+
+          <button onClick={() => toggleTheme()}>Mudar tema</button>
+          <NavLink className="navbar-brand" to="/" button onClick={() => logout()} > Logout </NavLink>
+          
+
       </nav>
     );
 }
