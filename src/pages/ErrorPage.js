598f273a9af53249b5334ea397ba1db3516e9fc6
raphaelrl86 <raphael.lacerda@gmail.com>
2023-03-17 14:13:51 +0400

    Cria error page

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,18 @@
+
+
+const ErrorPage = () => {
+    return ( 
+        <div className="container">
+        
+            <div className="py-6 bg-gray-100">
+                <div className= 'container-fluid'>
+                    <div className='text-center pb-lg-4 pt-5'>
+                        <h1>Página não encontrada</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default ErrorPage;
--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -3,10 +3,6 @@ import {useParams} from "react-router-dom"
 import { AuthContext } from "../context/auth.context"
 import axios from 'axios'
 import ProfileCard from '../components/ProfileCard'
-import CommentaryCard from '../components/CommentaryCard'
-
-
-
 
 const UserProfilePage = () => {
     
@@ -14,12 +10,9 @@ const UserProfilePage = () => {
     const {isLoading, loggedInUser} = useContext(AuthContext)
     const { userId } = useParams()
 
-    const headers = {
-           
+    const headers = {      
         'Authorization': `Bearer ${loggedInUser.jwt}`
-
     }
-  
     useEffect(() => {
         
         axios.get(`${process.env.REACT_APP_API_URL}/users/${userId}`, {headers})
@@ -29,7 +22,6 @@ const UserProfilePage = () => {
         .catch(err => console.log(err))
     }, [isLoading])
 
-
     if(!loggedInUser) {
         return <p>Logue no site...</p>
     }
@@ -37,46 +29,7 @@ const UserProfilePage = () => {
 
     return (
         <div className="container mb-4">
-            {/* <h1> Olá { !isLoading && user.name }, </h1> */}
             <ProfileCard user={user} key={user._id}/>
-
-            {/* <div className="row">
-                        { user.commentary.length > 0 && user.commentary.map(commentary => {
-                            return (
-                                <CommentaryCard commentary={commentary} key={commentary._id} />       
-                            )
-                        })}
-            </div> */}
-
-            {/* <Link className="btn btn-primary" to={`/edit/users/${user._id}`}>Editar</Link>
-            <div className="row">
-                <div className="col-3">
-                    <img width="100%" src={user.profileImage} alt="profileImage" />
-                </div> */}
-                {/* <div className="col-9">
-                     */}
-                
-                
-                    {/* <p>Nome: {user.name} {user.surname}</p> */}
-                    {/* <p>Email: {user.email}</p> */}
-                    {/* <p>Endereço: {user.address}</p> */}
-                    
-                    {/* <p>{user.commentary}</p> */}
-                {/* </div> */}
-            {/* </div> */}
-            {/* <div className="row">
-                <h2>Usuario</h2> */}
-                {/* <div className="row">
-                    { user.length > 0 && user.map(user => {
-                        return (
-                            <ProfileCard user={user} key={user._id} />       
-                        )
-                    })}
-                </div> */}
-            {/* </div> */}
-
-                
-
         </div>
     )
 }
